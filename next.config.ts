b61a150d9ec62a5e5b72aeae0caedcd7f1985b30
treasure <treasureuzoma650@gmail.com>
2025-01-06 23:23:11 +0100

    typo

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "analyticsapp.vercel.app",
+        hostname: "analytix.vercel.app",
         pathname: "/**"
       }
     ]
